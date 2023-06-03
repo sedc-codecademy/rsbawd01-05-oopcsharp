c8ea0867b9b4a4ee8f915260c886666c1001f5c4
Almir Vuk <almir.vuk@outlook.com>
2023-06-03 12:19:08 +0200

    Class 01 code added.

---
+++ SEDC.OOPCSHARP.Examples/OurFirstProject/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello world!");
+
+        Console.WriteLine("    *");
+        Console.WriteLine("   ***");
+        Console.WriteLine("  *****");
+        Console.WriteLine(" *******");
+        Console.WriteLine("*********");
+
+        Console.WriteLine();
+
+        Console.WriteLine("   *");
+        Console.WriteLine("   *");
+        Console.WriteLine("  ***");
+        Console.WriteLine(" * * *");
+        Console.WriteLine("   *");
+        Console.WriteLine("   *");
+        Console.WriteLine(" *   *");
+        Console.WriteLine("*     *");
+    }
+}
