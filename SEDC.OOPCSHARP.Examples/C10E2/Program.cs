c9fc20f7f1127554c6a6b21e6491a09efcda0df6
Almir Vuk <almir.vuk@outlook.com>
2023-06-26 18:45:04 +0200

    Class 10 E2 code added.

--- SEDC.OOPCSHARP.Examples/C10E2/Program.cs
+++ SEDC.OOPCSHARP.Examples/C10E2/Program.cs
@@ -2,8 +2,58 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Simple Calculator\n");
+
+            // Get the first number from the user
+            Console.WriteLine("Enter the first number: ");
+            double num1 = int.Parse(Console.ReadLine());
+
+            // Get the operator from the user
+            Console.WriteLine("Enter the operator (+, -, *, /): ");
+            string oper = Console.ReadLine();
+
+            // Get the second number from the user
+            Console.WriteLine("Enter the second number: ");
+            double num2 = int.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            // Perform the arithmetic operation based on the operator
+            switch (oper)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid operator.");
+                    return;
+            }
+
+            // Display the result
+            Console.WriteLine(".....................");
+            Console.WriteLine($"Result: {result}");
+            Console.WriteLine(".....................");
+
+            Console.ReadLine();
         }
     }
 }
