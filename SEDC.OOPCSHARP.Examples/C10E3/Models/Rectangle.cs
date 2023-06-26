36638a79964782c33a55191cfa7edd930153b8ad
Almir Vuk <almir.vuk@outlook.com>
2023-06-26 18:49:56 +0200

    Class 10 E3 code added.

---
+++ SEDC.OOPCSHARP.Examples/C10E3/Models/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿namespace C10E3.Models
+{
+    class Rectangle
+    {
+        public int Length { get; set; }
+        public int Width { get; set; }
+
+        public int CalculateArea()
+        {
+            return Length * Width;
+        }
+    }
+}
--- SEDC.OOPCSHARP.Examples/C10E3/Program.cs
+++ SEDC.OOPCSHARP.Examples/C10E3/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace C10E3
+﻿using C10E3.Models;
+
+namespace C10E3
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Rectangle Area Calculator\n");
+
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Enter the length: ");
+            rectangle.Length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width: ");
+            rectangle.Width = int.Parse(Console.ReadLine());
+
+            // Calculate the area
+            int area = rectangle.CalculateArea();
+
+            // Display the area on the console
+            Console.WriteLine($"\nArea: {area}");
+
+            Console.ReadLine();
         }
-    }
+    }    
 }
